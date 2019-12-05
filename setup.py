27e52538a60e872ef289527e3b57f235464d32b6
Dimo Drangov <dimodrangov@gmail.com>
2019-12-05 08:31:27 +0200

    requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AdvancedNLP',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Dimo Drangov',
+    author_email='dimodrangov@gmail.com',
+    description='Some useful NLP Tasks'
+)
